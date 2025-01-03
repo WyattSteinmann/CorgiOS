433ed7c7461366fd9004024e2b2eadd0f6abfa8d
randomusert <127417963+randomusert@users.noreply.github.com>
2025-01-03 13:04:21 +0200

    fix typo

--- CorgiOS/Programs/ShellX.cs
+++ CorgiOS/Programs/ShellX.cs
@@ -15,7 +15,7 @@ namespace CorgiOS.Programs
             Console.BackgroundColor = ConsoleColor.Black;
             Console.ForegroundColor = ConsoleColor.Red;
             Console.Clear();
-            Console.WriteLine("Corgi OS Has Crashed Due To Unexpected Error.\nWe Are Sorry For The Error We Tryed Hard To Prevent This In Programing.\nWe Hope You Did Not Lose Any Work.\nYou Can Support This Project To Prevent Errors Like This.\nThis Error Came From File ShellX.cs From The SRC Code.\n\nError Code: " + code + "\nInfo: " + info);
+            Console.WriteLine("Corgi OS Has Crashed Due To Unexpected Error.\nWe Are Sorry For The Error We Tried Hard To Prevent This In Programing.\nWe Hope You Did Not Lose Any Work.\nYou Can Support This Project To Prevent Errors Like This.\nThis Error Came From File ShellX.cs From The SRC Code.\n\nError Code: " + code + "\nInfo: " + info);
             Drivers.Audio.PlayBeep(1000, 1000);
             Thread.Sleep(10000);
             ACPIManager.Reboot();
